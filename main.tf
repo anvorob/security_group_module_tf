50b28cde03cdccf89ea26f3355cb21ada6a5c317
Anatolii Vorobiov <anatolii.vorobiov@gmail.com>
2026-01-31 21:19:40 +1300

    Refactor security group rules to use each.value

--- main.tf
+++ main.tf
@@ -11,17 +11,19 @@ resource "aws_security_group" "main" {
 resource "aws_vpc_security_group_ingress_rule" "ingress_rule" {
   for_each = var.ingress_rules
   security_group_id = aws_security_group.main.id
-  cidr_ipv4         = var.cidr_ipv4
-  from_port         = var.from_port
-  ip_protocol       = var.ip_protocol
-  to_port           = var.to_port
+  cidr_ipv4         = each.value.cidr_ipv4
+  referenced_security_group_id = each.value.source_security_group_id
+  from_port         = each.value.from_port
+  ip_protocol       = each.value.ip_protocol
+  to_port           = each.value.to_port
 }
 
 resource "aws_vpc_security_group_egress_rule" "egress_rule" {
   for_each = var.egress_rules
   security_group_id = aws_security_group.main.id
-  cidr_ipv4         = var.cidr_ipv4
-  from_port         = var.from_port
-  ip_protocol       = var.ip_protocol
-  to_port           = var.to_port
+  cidr_ipv4         = each.value.cidr_ipv4
+  referenced_security_group_id = each.value.source_security_group_id
+  from_port         = each.value.from_port
+  ip_protocol       = each.value.ip_protocol
+  to_port           = each.value.to_port
 }
