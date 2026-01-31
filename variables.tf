e45fe2975fbeba55d03732e64f56f6c445c45cb5
Anatolii Vorobiov <anatolii.vorobiov@gmail.com>
2026-01-31 20:12:01 +1300

    Update security group variables structure
    Refactor security group variables to include nested ingress and egress rules.

--- variables.tf
+++ variables.tf
@@ -1,37 +1,56 @@
-variable "name" {
-  type = string
-  description = "Security group name"
-}
-
-variable "description"
-{
-  type = string
-  description = "Security group description"
-}
-variable "vpc_id"
-{
-  type = string
-  description = "VPC ID where SG will be added to"
-}
-
-variable "ingress_rules"
+variable "security_groups"
 {
   type = map(object({
-    cidr_ipv4         = string
-    from_port         = number
-    ip_protocol       = string
-    to_port           = number
+    name = string
+    description = string
+    vpc_id = string
+    ingress_rules = map(object({
+      cidr_ipv4         = string
+      from_port         = number
+      ip_protocol       = string
+      to_port           = number
+    }))
+    egress_rules = map(object({
+      cidr_ipv4         = string
+      from_port         = number
+      ip_protocol       = string
+      to_port           = number
+    }))
   }))
-  
 }
 
-variable "egress_rules"
-{
-  type = map(object({
-    cidr_ipv4         = string
-    from_port         = number
-    ip_protocol       = string
-    to_port           = number
-  }))
-  
-}
+#variable "name" {
+#  type = string
+#  description = "Security group name"
+#}
+
+#variable "description"
+#{
+#  type = string
+#  description = "Security group description"
+#}
+#variable "vpc_id"
+#{
+#  type = string
+#  description = "VPC ID where SG will be added to"
+#}
+
+#variable "ingress_rules"
+#{
+#  type = map(object({
+#    cidr_ipv4         = string
+#    from_port         = number
+#    ip_protocol       = string
+#    to_port           = number
+#  }))  
+#}
+
+#variable "egress_rules"
+#{
+#  type = map(object({
+#    cidr_ipv4         = string
+#    from_port         = number
+#    ip_protocol       = string
+#    to_port           = number
+#  }))
+#}
