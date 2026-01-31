c3c64e9eb41addbb818a07743a2b80319539678f
Anatolii Vorobiov <anatolii.vorobiov@gmail.com>
2026-01-31 21:24:58 +1300

    Update security group variable definitions
    Refactor security group variables for clarity and structure.

--- variables.tf
+++ variables.tf
@@ -1,58 +1,60 @@
-variable "security_groups"
-{
-  type = map(object({
-    name = string
-    description = string
-    vpc_id = string
-    ingress_rules = map(object({
-      cidr_ipv4         = string
-      source_security_group_id = string
-      from_port         = number
-      ip_protocol       = string
-      to_port           = number
-    }))
-    egress_rules = map(object({
-      cidr_ipv4         = string
-      source_security_group_id = string
-      from_port         = number
-      ip_protocol       = string
-      to_port           = number
-    }))
-  }))
-}
-
-#variable "name" {
-#  type = string
-#  description = "Security group name"
-#}
-
-#variable "description"
-#{
-#  type = string
-#  description = "Security group description"
-#}
-#variable "vpc_id"
-#{
-#  type = string
-#  description = "VPC ID where SG will be added to"
-#}
-
-#variable "ingress_rules"
+#variable "security_groups"
 #{
 #  type = map(object({
-#    cidr_ipv4         = string
-#    from_port         = number
-#    ip_protocol       = string
-#    to_port           = number
-#  }))  
-#}
-
-#variable "egress_rules"
-#{
-#  type = map(object({
-#    cidr_ipv4         = string
-#    from_port         = number
-#    ip_protocol       = string
-#    to_port           = number
+#    name = string
+#    description = string
+#    vpc_id = string
+#    ingress_rules = map(object({
+#      cidr_ipv4         = string
+#      source_security_group_id = string
+#      from_port         = number
+#      ip_protocol       = string
+#      to_port           = number
+#    }))
+#    egress_rules = map(object({
+#      cidr_ipv4         = string
+#      source_security_group_id = string
+#      from_port         = number
+#      ip_protocol       = string
+#      to_port           = number
+#    }))
 #  }))
 #}
+
+variable "name" {
+  type = string
+  description = "Security group name"
+}
+
+variable "description"
+{
+  type = string
+  description = "Security group description"
+}
+variable "vpc_id"
+{
+  type = string
+  description = "VPC ID where SG will be added to"
+}
+
+variable "ingress_rules"
+{
+  type = map(object({
+    source_security_group_id = string
+    cidr_ipv4         = string
+    from_port         = number
+    ip_protocol       = string
+    to_port           = number
+  }))  
+}
+
+variable "egress_rules"
+{
+  type = map(object({
+    source_security_group_id = string
+    cidr_ipv4         = string
+    from_port         = number
+    ip_protocol       = string
+    to_port           = number
+  }))
+}
